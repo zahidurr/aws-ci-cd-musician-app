30f96b4fc2c220f1faa68d1031e267bd49f8aa6c
Zahidur <zahidur.r@outlook.com>
2020-08-16 18:56:30 +0200

    Add AWS s3 bucket

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css";
+import { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
-import './styles/index.css';
-import { createStore } from "redux";
-import reducer from "./reducers";
-import middleware from "./middleware";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import { composeWithDevTools } from 'redux-devtools-extension'
-import * as serviceWorker from './serviceWorker';
+import Root from "./components/Root";
 
-import App from "./App";
-const store = createStore(reducer, composeWithDevTools(middleware));
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
 
-ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
-  document.getElementById("root")
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    font-family: Montserrat, sans-serif;
+  }
+`;
+
+render(
+  <>
+    <GlobalStyle />
+    <Root />
+  </>,
+  document.getElementById("app")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
